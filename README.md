2f74828e14163843aaaa415772196da58a727e67
Alexandr Zinin <znalexandr@yandex.ru>
2020-08-05 23:41:27 +0300

    feat: app scripts

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 Fullstack application use MongoDB + Express + React + Node
 
-`// add the installation instructions`
+```
+yarn app:install - Установка всех зависимостей
+yarn app:dev - Запуск приложения в режиме development
+yarn app:start - Запуск приложения в режиме production
+
+yarn backend:start - Запуск серверной части
+yarn frontend:dev - Запуск клиентской части в режиме разработки
+yarn frontend:build - Сборка клиентской части
+```
--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,14 @@ app.use('/api/auth', require('./routes/auth.routes'));
 
 const PORT = config.get('port') || 5000;
 
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, '../frontend', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+  });
+}
+
 async function start() {
   try {
     await mongoose.connect(config.get('mongoUri'), {
